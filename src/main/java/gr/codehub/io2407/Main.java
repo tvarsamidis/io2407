1900d24a9a5d769401433bbbcf72a43c2684aced
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-07-19 10:08:32 +0300

    Initialize

---
+++ src/main/java/gr/codehub/io2407/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gr.codehub.io2407;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
